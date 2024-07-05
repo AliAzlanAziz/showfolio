f5accc6f832e7323e5e17aba4826fe4c7b2491d4
AliAzlanAziz <ali@retrocausal.ai>
2024-07-05 17:19:27 +0500

    password length must now be from 8 to 20 chars and must be alphanumeric

--- src/validators/userSignin.validator.ts
+++ src/validators/userSignin.validator.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express';
 const userSigninSchema = Joi.object({
     email: Joi.string().trim().email().optional(),
     username: Joi.string().trim().min(4).max(128).optional(),
-    password: Joi.string().trim().required()
+    password: Joi.string().trim().min(8).max(20).alphanum().required()
 }).or('email', 'username').messages({
     'object.missing': 'Either email or username must be present'
 });
