028f8503bddbcf84080facbf551208544ab26356
AliAzlanAziz <ali@retrocausal.ai>
2024-07-09 14:41:31 +0500

    fixing stripe payload bug

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -13,14 +13,14 @@ const Webhook = async (req: Request, res: Response) => {
   try{
     const signature = req.headers['stripe-signature'] as string;
     console.log('req.body')
-    console.log(req.body)
-    const event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET_KEY || '');
+    console.log(req.body.toString())
+    const event = stripe.webhooks.constructEvent(req.body.toString(), signature, process.env.STRIPE_WEBHOOK_SECRET_KEY || '');
 
     switch (event.type) {
       case 'payment_intent.succeeded':
-        // console.log('here in payment_intent.succeeded')
-        // const paymentIntentSuccess = event.data.object;
-        // console.log(paymentIntentSuccess)
+        console.log('here in payment_intent.succeeded')
+        const paymentIntentSuccess = event.data.object;
+        console.log(paymentIntentSuccess)
         break;
       case 'payment_intent.payment_failed':
         // console.log('here in payment_intent.payment_failed')
